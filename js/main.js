1d7cbcb2e021fe5e5ce7d27fe42ae6586d43ab13
SakharovaKsu <nagis38@gmail.com>
2023-07-01 22:37:07 +0300

    Решение задачек из codewars

--- js/main.js
+++ js/main.js
@@ -5,8 +5,17 @@
 
 // 2. obj-and-array - Решаю задачки для закрепления работ с объектами и массивами (по копированию, в том числе)
 
+// 3. method-sort - Решаю задачки с использованием метода - сортировки
+
+// 4. methods - Решение задач с использованием разных методов - фильтр, map, reduce, find, splice
+
+// 5. tasks - Просто разные задачи (работа со строками)
+
+// 6.  codeWars - задачи из codewars
+
 import * as arrays from './associative-arrays.js';
 import * as objArray from './obj-and-array.js';
 import * as sort from './method-sort.js'
 import * as methods from './methods.js'
 import * as tasks from './tasks.js'
+import * as codeWars from './codewars.js'
